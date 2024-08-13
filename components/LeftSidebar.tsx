440146fda3e3b9f9168dd4892fcd60d41e0a7f62
Laxman Rathod <laxmanrathod.dev@gmail.com>
2024-08-13 20:53:56 +0530

    Implement left sidebar component

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 
 const LeftSidebar = () => {
-  return (
-    <div>LeftSidebar</div>
-  )
-}
+  const route = useRouter();
+  const pathname = usePathname();
 
-export default LeftSidebar
+  return (
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
+          <Image src="/icons/logo.svg" width={23} height={27} alt="logo" />
+          <h1 className="text-24 font-extrabold text-white-1 max-lg:hidden">
+            Podly
+          </h1>
+        </Link>
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+
+          return (
+            <Link
+              key={label}
+              href={route}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
