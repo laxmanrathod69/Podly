e9a5f631322c808099b7b6ad4ebadbf50bf94dfb
Laxman Rathod <laxmanrathod.dev@gmail.com>
2024-08-14 23:05:27 +0530

    Implement convex setup

---
+++ convex/users.ts
@@ -0,0 +1,19 @@
+import { internalMutationGeneric } from "convex/server";
+import { v } from "convex/values";
+
+export const createUser = internalMutationGeneric({
+  args: {
+    clerkId: v.string(),
+    email: v.string(),
+    name: v.string(),
+    imageUrl: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("users", {
+      clerkId: args.clerkId,
+      email: args.email,
+      name: args.name,
+      imageUrl: args.imageUrl,
+    });
+  },
+});
