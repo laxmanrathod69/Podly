e7f7ca308e70e9a24a8d1fcdab8016ca7b243b22
Laxman Rathod <laxmanrathod.dev@gmail.com>
2024-08-14 17:59:33 +0530

    Add homepage layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import MobileNav from "@/components/MobileNav";
 
 export default function RootLayout({
   children,
@@ -6,10 +9,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-      <LeftSidebar />
-      {children}
-      <p className="text-white-1">RIGHT SIDEBAR</p>
-    </main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                alt="menu logo"
+                width={30}
+                height={30}
+              />
+              <MobileNav />
+            </div>
+            <div>
+              TOASTER (notification popus)
+              {children}
+            </div>
+          </div>
+        </section>
+        <RightSidebar />
+      </main>
+    </div>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+
 const Home = () => {
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Home</h1>
+        <div className="podcast_grid">
+          {podcastData.map(({ id, imgURL, title, description }) => (
+            <PodcastCard
+              key={id}
+              imgURL={imgURL}
+              title={title}
+              podcastId={id}
+              description={description}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
