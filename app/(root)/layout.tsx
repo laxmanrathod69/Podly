1c694985a9572d2f0902dee081f2bdaeb85c9f0b
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-01-25 04:57:09 +0530

    refactor: podcast player

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import LeftSidebar from "@/components/LeftSidebar";
 import RightSidebar from "@/components/RightSidebar";
 import MobileNav from "@/components/MobileNav";
 import { Toaster } from "@/components/ui/toaster";
-import PodcastPlayer from "@/components/PodcastPlayer";
+import PodcastPlayer from "@/components/global/podcast-player";
 
 export default function RootLayout({
   children,
